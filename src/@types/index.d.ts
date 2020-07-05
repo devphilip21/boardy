87254fa6881fc54a73234928bc722f741d788e32
load0ne <jjazan201@gmail.com>
2020-07-05 13:58:35 +0900

    [ FEATURE ] enhance Shape class #3
    - child method
    - notify method

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,5 +1,11 @@
 /* eslint-disable no-unused-vars */
-declare namespace Boardy {
-  export type Observer = (payload: any) => void
-  export type UnregisterFunc = () => void;
+declare namespace T {
+  export type ShapeType = 'fragment' | 'line' | 'point'
+  export type ObserverPayload = {
+    type: ShapeType;
+    payload: any;
+  };
+  export type Observer = (payload: ObserverPayload) => void
+  export type Unregister = () => void;
+  export type RemoveChild = () => void;
 }
