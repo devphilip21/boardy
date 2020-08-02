edcbecf6b76ea8e980e6277f26f382ee8c32a6f5
devphilip21 <philip21.dev@gmail.com>
2020-08-02 14:45:30 +0900

    [ CHORE ] add example - "extend tools" #18

--- examples/extend-tools/index.ts
+++ examples/extend-tools/index.ts
@@ -8,33 +8,20 @@ boardy.on((action) => {
   boardy.render(action);
 });
 
-// 1. create your custom tools!
-//   - args: { [actionType]: DrawingFunction }
-//     - actionType: declared in Boardy.ActionType
-//     - DrawingFunction: (
-//         ctx: CanvasContext,
-//         pointX: CoordinateX,
-//         pointY: CoordinateY,
-//         unit: OnePixelByResolution
-//       ) => void
+// 1. maybe parent tool exist.
+//   - see example[add-tools] for details.
 const redLine = Boardy.Tool.create({
-  // if mouse down, beginPath and set path style.
   [ActionType.MouseDown]: (ctx, pointX, pointY, unit) => {
     ctx.beginPath();
     ctx.moveTo(pointX, pointY);
     ctx.lineJoin = 'round';
-    // if you want to set it to 1px,
-    // use 1 * unit (multiply unit).
-    // it will be drawn in 1px that can correspond to various resolutions
     ctx.lineWidth = 1 * unit;
     ctx.strokeStyle = '#f00';
   },
-  // if mouse down and move, call lineTo and stroke.
   [ActionType.MouseDownAndMove]: (ctx, pointX, pointY) => {
     ctx.lineTo(pointX, pointY);
     ctx.stroke();
   },
-  // if mouse up or out, close path.
   [ActionType.MouseUp]: (ctx) => {
     ctx.closePath();
   },
@@ -44,9 +31,27 @@ const redLine = Boardy.Tool.create({
 });
 
 
+// 2. extends your custom tools!
+//   - args[0]: parent tool
+//   - args[1]: { [actionType]: DrawingFunction }
+//     - actionType: declared in Boardy.ActionType
+//     - DrawingFunction: (
+//         ctx: CanvasContext,
+//         pointX: CoordinateX,
+//         pointY: CoordinateY,
+//         unit: OnePixelByResolution
+//       ) => void
+const thickRedLine = Boardy.Tool.extend(redLine, {
+  // if mouse down, beginPath and set path style.
+  [ActionType.MouseDown]: (ctx, pointX, pointY, unit) => {
+    ctx.lineWidth = 10 * unit;
+  },
+});
+
+
 // 2. add tool with name.
 //   - you can use this tool by name
-boardy.addTool('redLine', redLine);
+boardy.addTool('thickRedLine', thickRedLine);
 
 // 3. use tool by name
-boardy.useTool('redLine');
+boardy.useTool('thickRedLine');
