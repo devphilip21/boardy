592ec3d9eceefce8979a5f5a6f2f227b1ccd3330
devphilip21 <philip21.dev@gmail.com>
2020-08-02 16:36:32 +0900

    [ CHORE ] add example - "app" #18

---
+++ examples/app/index.ts
@@ -0,0 +1,141 @@
+/* eslint-disable spaced-comment */
+import Boardy, {ActionType} from '@/Boardy';
+
+/////////////////////////////////////
+/////////// Server //////////////////
+/////////////////////////////////////
+
+// virtual socket module for example
+//   - to show like real-time communication
+const socket = createVirtualSocket();
+
+/////////////////////////////////////
+/////////// Leader Client ///////////
+/////////////////////////////////////
+
+// 1. initialize boardy module
+const leader = new Boardy({
+  canvas: document.querySelector('#canvas-leader'),
+});
+
+// 2. set drawing tool
+//    - blackline is default tool
+leader.useTool('blackline');
+
+// 3. if action is triggered,
+//    3.1. render on self canvas.
+//    3.2. send action using socket.
+leader.on((action) => {
+  leader.render(action);
+  socket.send(action);
+});
+
+
+////////////////////////////////////
+/////////// Member Clients /////////
+////////////////////////////////////
+
+const firstMember = new Boardy({
+  canvas: document.querySelector('#canvas-member-1'),
+});
+const secondMember = new Boardy({
+  canvas: document.querySelector('#canvas-member-2'),
+});
+
+socket.on((action) => {
+  firstMember.render(action);
+  secondMember.render(action);
+});
+
+
+////////////////////////////////////
+///////// Custom Tool //////////////
+////////////////////////////////////
+
+// create line tool (to use global color)
+//   - see example[add-tools] for details.
+let penColor = '#000';
+const globalColorLine = Boardy.Tool.create({
+  [ActionType.MouseDown]: (ctx, pointX, pointY, unit) => {
+    ctx.beginPath();
+    ctx.moveTo(pointX, pointY);
+    ctx.lineJoin = 'round';
+    // if you want to set it to 1px,
+    // use 1 * unit (multiply unit).
+    // it will be drawn in 1px that can correspond to various resolutions
+    ctx.lineWidth = 2 * unit;
+    ctx.strokeStyle = penColor;
+  },
+  // if mouse down and move, call lineTo and stroke.
+  [ActionType.MouseDownAndMove]: (ctx, pointX, pointY) => {
+    ctx.lineTo(pointX, pointY);
+    ctx.stroke();
+  },
+  // if mouse up or out, close path.
+  [ActionType.MouseUp]: (ctx) => {
+    ctx.closePath();
+  },
+  [ActionType.MouseOut]: (ctx) => {
+    ctx.closePath();
+  },
+});
+const lineToolName = 'globalColorLine';
+
+(function initTool() {
+  leader.addTool(lineToolName, globalColorLine);
+  firstMember.addTool(lineToolName, globalColorLine);
+  secondMember.addTool(lineToolName, globalColorLine);
+  allClientsUseTool(lineToolName);
+})();
+
+////////////////////////////////////
+/////////// View ///////////////////
+////////////////////////////////////
+
+const toolSelectElements = [
+  document.querySelector('#inp-pen'),
+  document.querySelector('#btn-eraser'),
+];
+
+highlightTool(0);
+
+toolSelectElements[0].addEventListener('input', (e: any) => {
+  penColor = e.target.value;
+  allClientsUseTool(lineToolName);
+  highlightTool(0);
+});
+
+toolSelectElements[1].addEventListener('click', (e: any) => {
+  allClientsUseTool('eraser');
+  highlightTool(1);
+});
+
+function highlightTool(index) {
+  toolSelectElements.forEach((el, i) => {
+    if (i === index) el.parentElement.classList.add('is-selected');
+    else el.parentElement.classList.remove('is-selected');
+  });
+}
+
+////////////////////////////////////
+/////////// Util ///////////////////
+////////////////////////////////////
+
+function allClientsUseTool(toolName) {
+  leader.useTool(toolName);
+  firstMember.useTool(toolName);
+  secondMember.useTool(toolName);
+}
+
+function createVirtualSocket() {
+  let communication;
+
+  return {
+    send(action) {
+      communication(action);
+    },
+    on(fn) {
+      communication = fn;
+    },
+  };
+}
